3ceaf5faffda806395771161c024042866a77c13
D.Senal Danushka Galagedara <senalgalagedara3@gmail.com>
2025-03-12 23:46:49 +0530

    senal

---
+++ book-store/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBooks from './pages/ShowBooks';
+import EditBooks from './pages/EditBooks';
+import DeleteBooks from './pages/DeleteBooks';
+const App = () => {
+  return (
+    <Routes>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+
+    </Routes>
+  )
+}
+
+export default App
