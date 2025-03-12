81b723734dd37f4445dd5b7098db2c7e8c80485f
D.Senal Danushka Galagedara <senalgalagedara3@gmail.com>
2025-03-12 23:02:24 +0530

    senal

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://senalgalagedara3:<db_password>root@cluster1.sfm2y.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster1';
+export const mongoDBURL = 'mongodb+srv://senalgalagedara3:gBJJSkriWSuyAOIu@book-store.y9fau.mongodb.net/books?retryWrites=true&w=majority&appName=book-store';
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import {PORT ,mongoDBURL} from "./config.js"
 import mongoose  from "mongoose";
 import {Book} from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
 const app = express();
 
 app.use(express.json());
@@ -10,104 +11,7 @@ app.get('/',(request, response)=>{
     return response.status(234).send("Hello Senal");
 });
 
-// create book db?
-app.post('/books',async(request, response)=>{
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all requred fields',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-        return response.status(200).send(book);
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-//get book by id?
-app.post('/books/:id',async(request, response)=>{
-    try{
-        const {id} = request.params;
-        const books = await Book.findByID({});
-
-        return response.status(200).json({
-            count : books.length,
-            data : books
-        });
-        return response.status(200).send(book);
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-app.listen(PORT, ()=>{
-    console.log(`APP is listening to port: ${PORT}`);
-});
-
-app.get('/books',async (request, response)=>{
-    try{
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({ message : error.message});
-    }
-});
-
-//update book
-app.put('./books/:id',async (request, response)=>{
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all requred fields',
-            });
-        }
-        const {id} = request.params;
-        const result = await Book.findByIdAndUpdate(id,request.body);
-        if(!result){
-            return response.status(404).json({message: 'Book not found'});
-        }
-        return response.status(200).send({message: 'Book updated successfully'});
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({ message : error.message});
-    }
-});
-
-//delete book
-app.delete('./books/:id',async (request, response)=>{
-    try{
-        const {id} = request.params;
-        const result = await Book.findByIdAndUpdate(id,request.body);
-        if(!result){
-            return response.status(404).json({message: 'Book not found'});
-        }
-        return response.status(200).send({message: 'Book deleted successfully'});
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({ message : error.message});
-    }
-});
+app.use('/books', booksRoute);
 mongoose
     .connect(mongoDBURL)
     .then(()=>{
